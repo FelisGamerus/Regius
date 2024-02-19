3b6e4ae5c88b067ba49bb1a470d3331303ca2ec9
FelisGamerus <felisgamerus@gmail.com>
2024-02-19 14:51:51 -0500

    Added dried sphagnum moss block class

---
+++ src/main/java/net/felisgamerus/regius/block/DriedSphagnumMossBlock.java
@@ -0,0 +1,9 @@
+package net.felisgamerus.regius.block;
+
+import net.minecraft.world.level.block.Block;
+
+public class DriedSphagnumMossBlock extends Block {
+    public DriedSphagnumMossBlock(Properties properties) {
+        super(properties);
+    }
+}
