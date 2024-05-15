7d61807017cefe57da2277fac9e39aa3a01b1506
FelisGamerus <felisgamerus@gmail.com>
2024-05-15 17:24:42 -0400

    Added hollow logs and change item registration

--- src/main/java/net/felisgamerus/regius/block/ModBlocks.java
+++ src/main/java/net/felisgamerus/regius/block/ModBlocks.java
@@ -1,9 +1,10 @@
 package net.felisgamerus.regius.block;
 
 import net.felisgamerus.regius.Regius;
-import net.felisgamerus.regius.item.ModItems;
-import net.minecraft.world.item.BlockItem;
-import net.minecraft.world.item.Item;
+import net.felisgamerus.regius.block.custom.DriedSphagnumMossBlock;
+import net.felisgamerus.regius.block.custom.DriedSphagnumPlant;
+import net.felisgamerus.regius.block.custom.FlammableHollowLog;
+import net.felisgamerus.regius.block.custom.SphagnumPlant;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.SoundType;
@@ -14,28 +15,27 @@ import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
 
-import java.util.function.Supplier;
-
 public class ModBlocks {
     public static final DeferredRegister<Block> BLOCKS =
             DeferredRegister.create(ForgeRegistries.BLOCKS, Regius.MOD_ID);
 
     //Sphagnum plants
-    public static final RegistryObject<Block> SPHAGNUM_MOSS = registerBlock("sphagnum_moss",
-            () -> new SphagnumPlant(BlockBehaviour.Properties.of().mapColor(MapColor.COLOR_GREEN).sound(SoundType.WET_GRASS).strength(0.1F).noOcclusion().noCollission()));
-    public static final RegistryObject<Block> DRIED_SPHAGNUM_MOSS = registerBlock("dried_sphagnum_moss",
-            () -> new DriedSphagnumPlant(SPHAGNUM_MOSS, BlockBehaviour.Properties.of().mapColor(MapColor.COLOR_YELLOW).sound(SoundType.GRASS).strength(0.1F).noOcclusion().noCollission()));
+    public static final RegistryObject<Block> SPHAGNUM_MOSS = BLOCKS.register("sphagnum_moss",
+            () -> new SphagnumPlant(BlockBehaviour.Properties.of().mapColor(MapColor.PLANT).sound(SoundType.WET_GRASS).strength(0.1F).noOcclusion().noCollission()));
+    public static final RegistryObject<Block> DRIED_SPHAGNUM_MOSS = BLOCKS.register("dried_sphagnum_moss",
+            () -> new DriedSphagnumPlant(SPHAGNUM_MOSS, BlockBehaviour.Properties.of().mapColor(MapColor.WOOD).sound(SoundType.GRASS).strength(0.1F).noOcclusion().noCollission()));
 
     //Sphagnum blocks
-    public static final RegistryObject<Block> SPHAGNUM_MOSS_BLOCK = registerBlock("sphagnum_moss_block",
-            () -> new Block(BlockBehaviour.Properties.copy(Blocks.MOSS_BLOCK).sound(SoundType.WET_GRASS)));
-    public static final RegistryObject<Block> DRIED_SPHAGNUM_MOSS_BLOCK = registerBlock("dried_sphagnum_moss_block",
-            () -> new DriedSphagnumMossBlock(SPHAGNUM_MOSS_BLOCK, BlockBehaviour.Properties.copy(Blocks.MOSS_BLOCK).mapColor(MapColor.COLOR_YELLOW).sound(SoundType.GRASS)));
+    public static final RegistryObject<Block> SPHAGNUM_MOSS_BLOCK = BLOCKS.register("sphagnum_moss_block",
+            () -> new Block(BlockBehaviour.Properties.copy(Blocks.MOSS_BLOCK).mapColor(MapColor.PLANT).sound(SoundType.WET_GRASS)));
+    public static final RegistryObject<Block> DRIED_SPHAGNUM_MOSS_BLOCK = BLOCKS.register("dried_sphagnum_moss_block",
+            () -> new DriedSphagnumMossBlock(SPHAGNUM_MOSS_BLOCK, BlockBehaviour.Properties.copy(Blocks.MOSS_BLOCK).mapColor(MapColor.WOOD).sound(SoundType.GRASS)));
 
-    private static <T extends Block> RegistryObject<T> registerBlock(String name, Supplier<T> block) {
-        RegistryObject<T> toReturn = BLOCKS.register(name, block);
-        return toReturn;
-    }
+    //Hollow logs
+    public static final RegistryObject<Block> HOLLOW_STRIPPED_OAK_LOG = BLOCKS.register("hollow_stripped_oak_log",
+            () -> new FlammableHollowLog(BlockBehaviour.Properties.copy(Blocks.OAK_LOG)));
+    public static final RegistryObject<Block> HOLLOW_OAK_LOG = BLOCKS.register("hollow_oak_log",
+            () -> new FlammableHollowLog(BlockBehaviour.Properties.copy(Blocks.OAK_LOG), false, HOLLOW_STRIPPED_OAK_LOG));
 
     public static void register(IEventBus eventBus) {
         BLOCKS.register(eventBus);
--- src/main/java/net/felisgamerus/regius/item/ModItems.java
+++ src/main/java/net/felisgamerus/regius/item/ModItems.java
@@ -1,16 +1,10 @@
 package net.felisgamerus.regius.item;
 
 import net.felisgamerus.regius.Regius;
-import net.felisgamerus.regius.block.DriedSphagnumMossBlock;
-import net.felisgamerus.regius.block.DriedSphagnumPlant;
 import net.felisgamerus.regius.block.ModBlocks;
+import net.felisgamerus.regius.item.custom.FuelBlockItem;
 import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.Item;
-import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.Blocks;
-import net.minecraft.world.level.block.SoundType;
-import net.minecraft.world.level.block.state.BlockBehaviour;
-import net.minecraft.world.level.material.MapColor;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
@@ -24,13 +18,19 @@ public class ModItems {
     public static final RegistryObject<Item> SPHAGNUM_MOSS = ITEMS.register("sphagnum_moss",
             () -> new BlockItem(ModBlocks.SPHAGNUM_MOSS.get(), new Item.Properties()));
     public static final RegistryObject<Item> DRIED_SPHAGNUM_MOSS = ITEMS.register("dried_sphagnum_moss",
-            () -> new BlockItem(ModBlocks.DRIED_SPHAGNUM_MOSS.get(), new Item.Properties()));
+            () -> new FuelBlockItem(ModBlocks.DRIED_SPHAGNUM_MOSS.get(), new Item.Properties(), 100));
 
     //Sphagnum blocks
     public static final RegistryObject<Item> SPHAGNUM_MOSS_BLOCK = ITEMS.register("sphagnum_moss_block",
             () -> new BlockItem(ModBlocks.SPHAGNUM_MOSS_BLOCK.get(), new Item.Properties()));
     public static final RegistryObject<Item> DRIED_SPHAGNUM_MOSS_BLOCK = ITEMS.register("dried_sphagnum_moss_block",
-            () -> new BlockItem(ModBlocks.DRIED_SPHAGNUM_MOSS_BLOCK.get(), new Item.Properties()));
+            () -> new FuelBlockItem(ModBlocks.DRIED_SPHAGNUM_MOSS_BLOCK.get(), new Item.Properties(), 1000));
+
+    //Hollow logs
+    public static final RegistryObject<Item> HOLLOW_STRIPPED_OAK_LOG = ITEMS.register("hollow_stripped_oak_log",
+            () -> new FuelBlockItem(ModBlocks.HOLLOW_STRIPPED_OAK_LOG.get(), new Item.Properties(), 300));
+    public static final RegistryObject<Item> HOLLOW_OAK_LOG = ITEMS.register("hollow_oak_log",
+            () -> new FuelBlockItem(ModBlocks.HOLLOW_OAK_LOG.get(), new Item.Properties(), 300));
 
     public static void register(IEventBus eventBus) {
         ITEMS.register(eventBus);
