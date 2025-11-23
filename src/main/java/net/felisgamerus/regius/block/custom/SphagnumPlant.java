a8dc5849acfc2367e0cbdf272e15eec2d2681239
FelisGamerus <felisgamerus@gmail.com>
2025-11-23 15:01:29 -0500

    Growable sphagnum

--- src/main/java/net/felisgamerus/regius/block/custom/SphagnumPlant.java
+++ src/main/java/net/felisgamerus/regius/block/custom/SphagnumPlant.java
@@ -2,10 +2,15 @@ package net.felisgamerus.regius.block.custom;
 
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.util.RandomSource;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.BlockGetter;
+import net.minecraft.world.level.Level;
 import net.minecraft.world.level.LevelAccessor;
 import net.minecraft.world.level.LevelReader;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.BonemealableBlock;
 import net.minecraft.world.level.block.SimpleWaterloggedBlock;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.StateDefinition;
@@ -17,8 +22,9 @@ import net.minecraft.world.phys.shapes.CollisionContext;
 import net.minecraft.world.phys.shapes.VoxelShape;
 
 //Remember to make this implement waterlogging later
-public class SphagnumPlant extends Block implements SimpleWaterloggedBlock {
+public class SphagnumPlant extends Block implements SimpleWaterloggedBlock, BonemealableBlock {
     public static final BooleanProperty WATERLOGGED = BlockStateProperties.WATERLOGGED;
+
     public SphagnumPlant(Properties properties) {
         super(properties);
         this.registerDefaultState(this.stateDefinition.any().setValue(WATERLOGGED, Boolean.valueOf(false)));
@@ -28,12 +34,15 @@ public class SphagnumPlant extends Block implements SimpleWaterloggedBlock {
         return canSupportRigidBlock(pLevel, pPos.below());
     }
 
+    //WATERLOGGING
     protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> pBuilder) {
         pBuilder.add(WATERLOGGED);
     }
+
     public FluidState getFluidState(BlockState pState) {
         return pState.getValue(WATERLOGGED) ? Fluids.WATER.getSource(false) : super.getFluidState(pState);
     }
+
     public BlockState updateShape(BlockState pState, Direction pFacing, BlockState pFacingState, LevelAccessor pLevel, BlockPos pCurrentPos, BlockPos pFacingPos) {
         if (pState.getValue(WATERLOGGED)) {
             pLevel.scheduleTick(pCurrentPos, Fluids.WATER, Fluids.WATER.getTickDelay(pLevel));
@@ -45,6 +54,19 @@ public class SphagnumPlant extends Block implements SimpleWaterloggedBlock {
     private static final VoxelShape SHAPE = box(0, 0, 0, 16, 5, 16);
     public VoxelShape getShape(BlockState pState, BlockGetter pLevel, BlockPos pPos, CollisionContext pContext) {
         return SHAPE;
+    }
+
+    //BONE MEAL
+    public boolean isValidBonemealTarget(LevelReader level, BlockPos pos, BlockState state) {
+        return true;
+    }
+
+    public boolean isBonemealSuccess(Level level, RandomSource random, BlockPos pos, BlockState state) {
+        return true;
+    }
+
+    public void performBonemeal(ServerLevel level, RandomSource random, BlockPos pos, BlockState state) {
+        popResource(level, pos, new ItemStack(this));
     }
 
     //Borrowed from the AbstractGlassBlock class
